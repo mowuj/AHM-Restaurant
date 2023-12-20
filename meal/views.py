22fc6ece8e702643ff637fc8b1c10f5030f6041f
mowuj <mowuj92@gmail.com>
2023-12-20 19:44:41 +0600

    done

--- meal/views.py
+++ meal/views.py
@@ -41,4 +41,4 @@ def meals(request):
                 "strCategoryDescription": "Pasta is a staple food of traditional Italian cuisine, with the first reference dating to 1154 in Sicily.\r\n\r\nAlso commonly used to refer to the variety of pasta dishes, pasta is typically a noodle made from an unleavened dough of a durum wheat flour mixed with water or eggs and formed into sheets or various shapes, then cooked by boiling or baking. As an alternative for those wanting a different taste, or who need to avoid products containing gluten, some pastas can be made using rice flour in place of wheat.[3][4] Pastas may be divided into two broad categories, dried (pasta secca) and fresh (pasta fresca)."
             },
         ]
-        return render(request,'show-items.html',{'data':categories})
+        return render(request,'index.html',{'data':categories})
